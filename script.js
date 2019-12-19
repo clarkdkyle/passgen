c18999030c72d24947d1e51b8650611d9ac0c07d
clarkdkyle <clarkdkyle@gmail.com>
2019-12-19 16:05:06 -0600

    correct prompts for keygen

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-function generatePassword() {
-  var length = 8;
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+<>";
+
+// password generate function
+function generatePassword() { 
   var retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
   }
   return retVal;
 }
+// variables for generatePassword funciton
+var length = prompt("How many characters should your password have? 8-128");
+var lwrcase = prompt("Would you like to include lower case letters? Yes or No?").toLowerCase()
+  if (lwrcase = "yes") {
+    lwrcase = "abcdefghijklmnopqrstuvwxyz"
+  } else {lwrcase = ""};
+var upprcase = prompt("Would you like to include upper case letters? Yes or No?" ).toLowerCase()
+  if (lwrcase = "yes") {
+    lwrcase = "abcdefghijklmnopqrstuvwxyz".toUpperCase()
+  }  else {lwrcase = ""};
+var symbols = prompt("Would you like to include Symbols? Yes or No?" ).toLowerCase()
+  if (symbols = "yes") {
+    symbols = "!@#$%^&*()_-+=~`:;.,><??/"
+  }  else {symbols = ""};
+var nmbrs = prompt("Would you like to include Numbers? Yes or No?" ).toLowerCase()
+  if (nmbrs = "yes") {
+    nmbrs = "1234567890"
+  }  else {nmbrs = ""};
+
+
+var charset = lwrcase + upprcase + symbols
 
 // Write password to the #password input
 function writePassword() {
