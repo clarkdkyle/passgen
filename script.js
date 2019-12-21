96edf4a11c83d0d5c3904291f5a9114e816298e6
clarkdkyle <clarkdkyle@gmail.com>
2019-12-20 23:29:56 -0600

    fixed prompt if statements

--- script.js
+++ script.js
@@ -9,27 +9,29 @@ function generatePassword() {
   }
   return retVal;
 }
+
+var charset = ""
 // variables for generatePassword funciton
 var length = prompt("How many characters should your password have? 8-128");
 var lwrcase = prompt("Would you like to include lower case letters? Yes or No?").toLowerCase()
-  if (lwrcase = "yes") {
-    lwrcase = "abcdefghijklmnopqrstuvwxyz"
-  } else {lwrcase = ""};
+  if (lwrcase === "yes") {
+    charset += "abscefghijklmnopqrstuvwxyz";
+  };
 var upprcase = prompt("Would you like to include upper case letters? Yes or No?" ).toLowerCase()
-  if (lwrcase = "yes") {
-    lwrcase = "abcdefghijklmnopqrstuvwxyz".toUpperCase()
-  }  else {lwrcase = ""};
+  if (upprcase === "yes") {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  };
 var symbols = prompt("Would you like to include Symbols? Yes or No?" ).toLowerCase()
-  if (symbols = "yes") {
-    symbols = "!@#$%^&*()_-+=~`:;.,><??/"
-  }  else {symbols = ""};
+  if (symbols === "yes") {
+    charset += "!@#$%^&*()_-+=~`:;.,><??/";
+  };
 var nmbrs = prompt("Would you like to include Numbers? Yes or No?" ).toLowerCase()
-  if (nmbrs = "yes") {
-    nmbrs = "1234567890"
-  }  else {nmbrs = ""};
+  if (nmbrs === "yes") {
+    charset += "1234567890";
+  };
 
 
-var charset = lwrcase + upprcase + symbols
+
 
 // Write password to the #password input
 function writePassword() {
@@ -37,16 +39,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
 }
 
 function copyToClipboard() {
   // BONUS 
+  var copyText = password;
+  copyText.select();
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// BONUS EVENT LISTENER
